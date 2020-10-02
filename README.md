fc868f61c909f10507df255b378f15429381b3ed
Yu Sasaki <m@yukun.info>
2020-10-02 01:03:21 +0000

    add file save function.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # http-status-code-checker
 Check the HTTP status codes (404 and 200) from bulk URLs.
+
+## 準備
+
+urls.txtにチェック対象URLを記載。1行1URL。
+
+例: 
+~~~
+https://yukun.info
+https://yukun.info/kcfinder/browse.php
+・
+・
+・
+~~~
+
+## 実行結果
+
+"数字".txtの形式でURLのHTML Body部が保管
+~~~
+1.txt
+2.txt
+・
+・
+・
+~~~
+
+result.txtにHTTP status code結果と対象URLが','区切りで出力
+
+~~~
+200,https://yukun.info
+404,https://yukun.info/kcfinder/browse.php
+~~~
--- check_spider.py
+++ check_spider.py
@@ -7,6 +7,17 @@ try:
 except:
     url_list = []
 
+i = 1
+result_list: List[str] = []
 for url in url_list:
     response = requests.get(url)
     print(f"{response.status_code},{url}")
+    result_list.append(str(response.status_code) + "," + url)
+    filename = str(i) + ".txt"
+    with open(filename, "w", encoding = "utf_8") as file:
+        file.write(response.text)
+    i = i + 1
+
+with open("result.txt", "w", encoding = "utf_8") as file:
+    for result in result_list:
+        file.write("%s\n" % result)
